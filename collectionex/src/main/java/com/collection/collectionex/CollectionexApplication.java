2b0b60dafe99bfa536ed172b01c416f32a05353c
Developer <developer@Developers-Mac-mini.local>
2024-01-18 21:22:13 +0530

    collection example start

---
+++ collectionex/src/main/java/com/collection/collectionex/CollectionexApplication.java
@@ -0,0 +1,13 @@
+package com.collection.collectionex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionexApplication.class, args);
+	}
+
+}
